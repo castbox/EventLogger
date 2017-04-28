a3f800ad836d0b1a40ced2506d1244ccb9e2c36c
xiaocong <xiaocong@gmail.com>
2017-04-28 10:57:16 +0800

    refine import

--- library/src/main/java/fm/castbox/eventlogger/EventLogger.java
+++ library/src/main/java/fm/castbox/eventlogger/EventLogger.java
@@ -9,7 +9,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
-import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsConstants;
 import com.facebook.appevents.AppEventsLogger;
 import com.google.android.gms.analytics.GoogleAnalytics;
