5263a78f9018df735fd6b70a5cf6aaae62982eb5
xiaocong <xiaocong@gmail.com>
2017-07-05 20:01:49 +0800

    add attribution

--- library/src/main/java/fm/castbox/eventlogger/CampaignTrackingReceiver.java
+++ library/src/main/java/fm/castbox/eventlogger/CampaignTrackingReceiver.java
@@ -2,6 +2,7 @@ package fm.castbox.eventlogger;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -14,6 +15,7 @@ import timber.log.Timber;
 public class CampaignTrackingReceiver extends com.google.android.gms.analytics.CampaignTrackingReceiver {
 
     private static final String PLAY_STORE_REFERRER_KEY = "referrer";
+    private static final String PLAY_STORE_ATTRIBUTION_KEY = "attribution";
     private static final String PLAY_STORE = "store";
 
     @Override
@@ -22,21 +24,30 @@ public class CampaignTrackingReceiver extends com.google.android.gms.analytics.C
             String url = intent.getStringExtra(PLAY_STORE_REFERRER_KEY);
             if (!TextUtils.isEmpty(url)) {
                 EventLogger.getInstance().logEvent(PLAY_STORE, PLAY_STORE_REFERRER_KEY, url);
-                try {  // report ADS keyword of Google adWords
-                    String[] kvs = url.split("&");
-                    for (String kv: kvs) {
-                        String[] pair = kv.split("=");
-                        if (pair.length == 2 && TextUtils.equals(pair[0], "keyword")) {
-                            EventLogger.getInstance().logEvent(PLAY_STORE, "ad_keyword", pair[1]);
-                            break;
+                try {  // report attribution and keywords
+                    final Uri uri = Uri.parse(url);
+                    final String utmSource = uri.getQueryParameter("utm_source");
+                    final String utmMedium = uri.getQueryParameter("utm_medium");
+                    final String utmTerm = uri.getQueryParameter("utm_term");
+                    final String keyword = uri.getQueryParameter("keyword");
+                    if (TextUtils.isEmpty(utmSource) && TextUtils.isEmpty(utmMedium)) {
+                        if (!TextUtils.isEmpty(keyword)) {
+                            EventLogger.getInstance().logEvent(PLAY_STORE, "keyword", keyword);
+                            EventLogger.getInstance().logEvent(PLAY_STORE, PLAY_STORE_ATTRIBUTION_KEY, "google.adwords");
                         }
+                    } else if (!TextUtils.isEmpty(utmSource)) {
+                        if (TextUtils.isEmpty(utmMedium))
+                            EventLogger.getInstance().logEvent(PLAY_STORE, PLAY_STORE_ATTRIBUTION_KEY, utmSource);
+                        else
+                            EventLogger.getInstance().logEvent(PLAY_STORE, PLAY_STORE_ATTRIBUTION_KEY, utmSource + "." + utmMedium);
+                        if (!TextUtils.isEmpty(utmTerm))
+                            EventLogger.getInstance().logEvent(PLAY_STORE, "term", utmTerm);
                     }
                 } catch (Exception e) {
                     Timber.e(e, "Invalid referrer.");
                 }
                 EventLogger.getInstance().setCampaignParams(url);
-
-                Log.e("CampaignTracking", "=====> referrer=" + url);
+                Log.d("CampaignTracking", "=====> referrer=" + url);
             }
         }
 
