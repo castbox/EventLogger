ac253c70f25d33f14e263ffd9a3c13805e14b619
faxi.wang <faxi.wang@castbox.fm>
2019-11-11 16:12:00 +0800

    update to androidx

--- library/src/main/java/fm/castbox/eventlogger/EventLogger.java
+++ library/src/main/java/fm/castbox/eventlogger/EventLogger.java
@@ -5,8 +5,8 @@ import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import android.text.TextUtils;
 
 import com.facebook.GraphRequest;
@@ -22,7 +22,6 @@ import java.net.URLDecoder;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.BiConsumer;
 
 import timber.log.Timber;
 
